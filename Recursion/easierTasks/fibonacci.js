f115d7fff6fcbcc31f12141df3f211d8c7b64a26
Abdulaziz <abdulazizfaactory@gmail.com>
2022-09-25 18:50:00 +0500

    recursion and  easier tasks

---
+++ Recursion/easierTasks/fibonacci.js
@@ -0,0 +1,17 @@
+function fib(num){
+    if(num === 1 || num === 2) {
+        return 1;
+    }
+    if(num <= 0) {
+        return 0;
+    }
+    
+    return fib(num - 1) + fib(num - 2);
+}
+
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
+
+console.log(fib(28));
