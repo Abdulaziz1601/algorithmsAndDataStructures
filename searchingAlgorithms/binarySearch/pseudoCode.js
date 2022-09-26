bf6ee3d09db7d497944d8f51987053b2471a567c
Abdulaziz <abdulazizfaactory@gmail.com>
2022-09-26 18:58:55 +0500

    binary search implementation

---
+++ searchingAlgorithms/binarySearch/pseudoCode.js
@@ -0,0 +1,11 @@
+// Function accepts a sorted array and a value
+
+// Create a left pointer at the start of the array, and a right pointer at the end of the array
+
+// While Left comes before Right pointer, loop
+    // Create a pointer at the middle 
+    // If you find the value you want, return the index
+    // If the value is too small, move the left pointer up
+    // If the value is too large, move the right pointer down
+
+    // If you never find the value, return -1
