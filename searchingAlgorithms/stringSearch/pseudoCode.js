c13d2fae2561fcbc6f1f800537b0fb63e4cf2d3f
Abdulaziz <abdulazizfaactory@gmail.com>
2022-09-27 23:49:36 +0500

    naive string search

---
+++ searchingAlgorithms/stringSearch/pseudoCode.js
@@ -0,0 +1,7 @@
+// Loop Over the longer string
+// Loop over the shorter string
+// If the charachters don't match, break out of the inner loop
+// If the characters do match, keep going
+// If you complete the inner loop and find a match
+    // Increment the count of matches
+// Return the count at the end
