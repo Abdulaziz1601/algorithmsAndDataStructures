16dc5d8c5309e9ac1ebbb309219c90154da7aa68
Abdulaziz <abdulazizfaactory@gmail.com>
2022-10-02 14:31:59 +0500

    insertion Sort

---
+++ SortingAlgorithms/insertionSort/insertionSortPseudocode.js
@@ -0,0 +1,11 @@
+// We gradually build up
+// Sorted portion of the array
+// By inserting one item at a time at the correct place
+
+// Pseudocode
+
+// We start by pciking the second element in the array
+// Now compare the second element with the one before It and swap if necessary 
+// Continue to the next element and check if It is in the correct order
+// we iterate through the sorted portion (i.e the left side) to place the element in the correct place
+// Repeat until array is sorted
