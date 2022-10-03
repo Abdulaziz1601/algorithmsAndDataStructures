85fa6ad0de2762939ac3ee667c0c64c051ac656f
Abdulaziz <abdulazizfaactory@gmail.com>
2022-10-03 13:02:26 +0500

    merging two sorted arrays, with time complexity O(N+M), Colt's solution

--- SortingAlgorithms/mergeSort/merging.js
+++ SortingAlgorithms/mergeSort/merging.js
@@ -1,18 +1,28 @@
 function merge(first, second) {
-    const res = [];
+    const results = [];
     let i = 0,
         j = 0;
 
     while (i < first.length && j < second.length) {
-        if (first[i] < second[j]) res.push(first[i++]);
-        else res.push(second[j++]);
-        console.log(res);
+        if (second[j] > first[i]) {
+            results.push(first[i]);
+            i++;
+        } else {
+            results.push(second[j]);
+            j++;
+        }
+    }
+    while (i < first.length) {
+        results.push(first[i]);
+        i++;
     }
 
-    i < first.length
-        ? res.push(...first.slice(i))
-        : res.push(...second.slice(j));
+    while (j < second.length) {
+        results.push(second[j]);
+        j++;
+    }
 
-    return res;
+    return results;
 }
-console.log(merge([500, 600, 700], [100, 105, 800]));
+console.log(merge([100], [4, 6, 122, 244]));
+
